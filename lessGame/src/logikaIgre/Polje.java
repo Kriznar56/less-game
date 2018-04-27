5002cc153c14a2411c1f660907ca3fbc59706ad2
Gal Bumbar <gal.bumbar@gmail.com>
2018-04-27 18:30:04 +0200

    fix, sam ne vem kaj so te <<<< HEAD in podobno

--- lessGame/src/logikaIgre/Polje.java
+++ lessGame/src/logikaIgre/Polje.java
@@ -3,7 +3,11 @@ package logikaIgre;
 public enum Polje {
 	PRAZNO, BELO, CRNO;
 	// vzeto iz primera.
-	// polje potrebno prilagodit za ovire. vsakemu polju bi dodal atribute zgoraj, spodaj, levo in desno, ki bo boolean, ki pove, ali je tam ovira. posledično polje ne bo več enum ampak ??.
+	// polje potrebno prilagodit za ovire. vsakemu polju bi dodal atribute zgoraj, spodaj, levo in desno, ki bo boolean, ki pove, ali je tam ovira. posledično polje ne bo več enum ampak ??
+	public boolean ovira_zgoraj = false;
+	public boolean ovira_spodaj = false;
+	public boolean ovira_levo = false;
+	public boolean ovira_desno = false;
 	public String toString() {
 		switch(this) {
 		case PRAZNO: return " ";
--- lessGame/src/logikaIgre/Stanje.java
+++ lessGame/src/logikaIgre/Stanje.java
@@ -5,9 +5,6 @@ public enum Stanje {
 	NA_POTEZI_CRN,
 	ZMAGAL_CRN,
 	ZMAGAL_BEL;
-	public boolean ovira_zgoraj = false;
-	public boolean ovira_spodaj = false;
-	public boolean ovira_levo = false;
-	public boolean ovira_desno = false;
+
 	//Neodloceno se mi zdi da ni možno? + razlog za zmago vedno enak, torej tudi tisti drug del v primeru ni proteben
 }
