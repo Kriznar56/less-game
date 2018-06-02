509fcb586d88227e199663108745ae2674bceeea
Gal Bumbar <gal.bumbar@gmail.com>
2018-06-02 20:14:11 +0200

    ideje za izboljšavo ocene

--- lessGame/src/inteligenca/Ocena.java
+++ lessGame/src/inteligenca/Ocena.java
@@ -13,14 +13,21 @@ public class Ocena {
 	public static final int ZGUBA = -ZMAGA;
 	private static LinkedList<Point> mojaPolja = new LinkedList<Point>();
 	private static LinkedList<Point> nasprotnikovaPolja = new LinkedList<Point>();
-
-	private static int vrednostPozicije(boolean b, Point p) {
+/*TO DO
+ * upoštevaj sosednje ploščke, več ko jih imaš v smeri cilja bolje je
+ * upoštevaj nasprotnikovo stanje in ga dodaj kot 'negativne točke'
+ * trenutno maksimizira ceno do svojega doma, namest da bi minimiziral ceno do cilja <- popravi
+*/
+	private static int vrednostPozicije(boolean b, Point p, Igra igra) {
 		//racunamo razdaljo ce smo beli do zgornjega desnega kota in ce smo crni do spodnjega levega
-		if(b) {
-			return (5+(int)p.getX()-(int)p.getY());
+		if(b) {//BELI
+			//return (5+(int)p.getX()-(int)p.getY());
+			
+			return igra.cenaPoteze((int)p.getX(), (int)p.getY(), 0, 5); //test s trenutno ceno poteze <- neoptimalno
 		}
-		else {
-			return (5-(int)p.getX()+(int)p.getY());
+		else {//CRNI
+			//return (5-(int)p.getX()+(int)p.getY());
+			return igra.cenaPoteze((int)p.getX(), (int)p.getY(), 5, 0); //test s trenutno ceno poteze <- neoptimalno
 		}
 	}
 	
@@ -59,10 +66,10 @@ public class Ocena {
 			}
 			//Najprej bomo ocenili samo tako da bomo pogledali koliko ima vsak ploscek x in y razdaljo do zgornjega ali spodnjega kota
 			for(Point p: mojaPolja) {
-				vrednostCRNI += vrednostPozicije(false, p);
+				vrednostCRNI += vrednostPozicije(false, p, igra);
 			}
 			for(Point p: nasprotnikovaPolja) {
-				vrednostBELI += vrednostPozicije(true, p);
+				vrednostBELI += vrednostPozicije(true, p, igra);
 			}
 			//System.out.println("ocena je "+(jaz==Igralec.BEL ? vrednostBELI : vrednostCRNI));
 			return (jaz==Igralec.BEL ? vrednostBELI : vrednostCRNI);
